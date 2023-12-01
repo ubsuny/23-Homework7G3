e7a44ad7333d2d0405b1fd5d381e7097998ba535
poojashresthacode <143756553+poojashresthacode@users.noreply.github.com>
2023-12-01 09:21:00 +0545

    Remove unused import.

--- configuration.py
+++ configuration.py
@@ -1,11 +1,8 @@
 """
 Module docstring: Provide a brief overview of the module here.
 """
-
-import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 import itertools
+import numpy as np
 import scipy.optimize
 
 KE2 = 197 / 137  # eV-nm Coulomb force charge
